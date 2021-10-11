15cb2868b1fd6eb1bda160a24eb5e743b5d9b9ba
vagrant <vagrant@homestead>
2021-10-11 04:22:13 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,10 +3,24 @@
 <html>
   <head>
     <title>@yield('title','running App') - Laravel 入门二刷</title>
+    {{-- Laravel 在运行时，是以 public 文件夹为根目录的，因此我们可以使用下面这行代码来为 Laravel 引入样式： --}}
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    {{-- 该占位区域将用于显示 content 区块的内容，而 content 区块的内容将由继承自 default 视图的子视图定义 --}}
-    @yield('content')
+    {{-- navbar 导航栏 --}}
+    <nav class="navbar navbar-expend-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">running App</a>
+        <u1 class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <il class="nav-item"><a class="nav-link" href="#">登录</a></il>
+        </u1>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
 
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,20 @@
 @section('title','主页')
 
 @section('content')
-  <h1>主页</h1>
+  {{-- jumbotron 超大屏幕 --}}
+  <div class="jumbotron">
+    <h1>Hello Laravel,this's my second time!</h1>
+    {{-- lead 领导 ;essential 必不可少的 training 培养，训练--}}
+    <p class="lead">
+      你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">第二遍的Laravel 入门教程</a>的running项目主页。
+    </p>
+    <p>
+      一切，又将重新开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册！</a>
+    </p>
+  </div>
 @stop
 
 
